7a3951b13abb91caca87b20f6a28a47428fb9146
Jens Becker <info@jhb-software.dev>
2024-05-14 07:12:24 +0200

    add annotation to model

--- lib/brick/models/project.model.dart
+++ lib/brick/models/project.model.dart
@@ -1,16 +1,18 @@
 import 'package:brick_offline_first_with_rest/brick_offline_first_with_rest.dart';
 import 'package:brick_sqlite/brick_sqlite.dart';
 
-import '../utils/uuid.dart';
-
+@ConnectOfflineFirstWithRest()
 class Project extends OfflineFirstWithRestModel {
   Project({
     required this.name,
     required this.id,
   });
 
+  //@Sqlite(index: true, unique: true)
+  //final UUID id;
+
   @Sqlite(index: true, unique: true)
-  final UUID id;
+  final String id;
 
   final String name;
 }
