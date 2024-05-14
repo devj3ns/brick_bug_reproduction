fd034ef3c07b9c361c8174d3c403ce1248509752
Jens Becker <info@jhb-software.dev>
2024-05-14 07:28:18 +0200

    use absolute import in model

--- lib/brick/models/project.model.dart
+++ lib/brick/models/project.model.dart
@@ -1,5 +1,6 @@
 import 'package:brick_offline_first_with_rest/brick_offline_first_with_rest.dart';
 import 'package:brick_sqlite/brick_sqlite.dart';
+import 'package:brick_test/brick/utils/uuid.dart';
 
 @ConnectOfflineFirstWithRest()
 class Project extends OfflineFirstWithRestModel {
@@ -8,11 +9,11 @@ class Project extends OfflineFirstWithRestModel {
     required this.id,
   });
 
-  //@Sqlite(index: true, unique: true)
-  //final UUID id;
-
   @Sqlite(index: true, unique: true)
-  final String id;
+  final UUID id;
+
+  // @Sqlite(index: true, unique: true)
+  // final String id;
 
   final String name;
 }
